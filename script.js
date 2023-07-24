38cd6d76a4c25d2a58b2d9a829298b4ec44aeb04
DaniloBM1 <140429439+DaniloBM1@users.noreply.github.com>
2023-07-24 14:16:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Números de matrículas autorizadas
 const numerosMatriculasAutorizadas = ["297684550674", "212210605005", "238946972429"];
 
+// Variável para armazenar os números dos armários disponíveis e indisponíveis
+let armariosDisponiveis = [1, 2, 5, 10, 15];
+let armariosIndisponiveis = [3, 4, 6, 8, 12];
+
 // Função para validar o número de matrícula
 function validarMatricula() {
   const matriculaInput = document.getElementById('matricula');
@@ -22,8 +26,45 @@ function exibirInformacoesArmarios() {
   statusContainer.innerHTML = `
     <h2>Status dos Armários</h2>
     <p>Total de armários: 100</p>
-    <p>Armários disponíveis: 1, 2, 5, 10, 15</p>
-    <p>Armários indisponíveis: 3, 4, 6, 8, 12</p>
+    <p>Armários disponíveis: ${armariosDisponiveis.join(", ")}</p>
+    <p>Armários indisponíveis: ${armariosIndisponiveis.join(", ")}</p>
   `;
   statusContainer.style.display = 'block';
+
+  const botoesContainer = document.createElement('div');
+  botoesContainer.classList.add('botoes-container');
+
+  armariosDisponiveis.forEach((armario) => {
+    const botao = document.createElement('button');
+    botao.textContent = `Marcar Armário ${armario} como Indisponível`;
+    botao.addEventListener('click', () => marcarArmarioIndisponivel(armario));
+    botoesContainer.appendChild(botao);
+  });
+
+  armariosIndisponiveis.forEach((armario) => {
+    const botao = document.createElement('button');
+    botao.textContent = `Marcar Armário ${armario} como Disponível`;
+    botao.addEventListener('click', () => marcarArmarioDisponivel(armario));
+    botoesContainer.appendChild(botao);
+  });
+
+  statusContainer.appendChild(botoesContainer);
+}
+
+// Função para marcar um armário como indisponível
+function marcarArmarioIndisponivel(armario) {
+  if (armariosDisponiveis.includes(armario)) {
+    armariosDisponiveis = armariosDisponiveis.filter((num) => num !== armario);
+    armariosIndisponiveis.push(armario);
+    exibirInformacoesArmarios(); // Atualizar as informações após a alteração
+  }
+}
+
+// Função para marcar um armário como disponível
+function marcarArmarioDisponivel(armario) {
+  if (armariosIndisponiveis.includes(armario)) {
+    armariosIndisponiveis = armariosIndisponiveis.filter((num) => num !== armario);
+    armariosDisponiveis.push(armario);
+    exibirInformacoesArmarios(); // Atualizar as informações após a alteração
+  }
 }
