7a66ba360d168c2094606594fb671c943ab17500
DaniloBM1 <140429439+DaniloBM1@users.noreply.github.com>
2023-07-24 14:56:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-// Números de matrículas autorizadas
-const numerosMatriculasAutorizadas = ["297684550674", "212210605005", "238946972429"];
+// Números de matrículas autorizadas e seus respectivos nomes
+const alunos = {
+  "297684550674": "João da Silva",
+  "212210605005": "Maria Souza",
+  "238946972429": "Pedro Almeida"
+};
 
 // Variável para armazenar os números dos armários disponíveis e indisponíveis
 let armariosDisponiveis = [1, 2, 5, 10, 15];
 let armariosIndisponiveis = [3, 4, 6, 8, 12];
+
+// Variável para armazenar o número de matrícula selecionado para marcar disponível para indisponível
+let matriculaSelecionada;
 
 // Função para validar o número de matrícula
 function validarMatricula() {
   const matriculaInput = document.getElementById('matricula');
   const matricula = matriculaInput.value.trim();
 
-  if (numerosMatriculasAutorizadas.includes(matricula)) {
+  if (alunos.hasOwnProperty(matricula)) {
     // Número de matrícula válido
+    matriculaSelecionada = matricula;
     matriculaInput.value = ''; // Limpar o campo de entrada
     exibirInformacoesArmarios();
   } else {
@@ -20,7 +28,7 @@ function validarMatricula() {
   }
 }
 
-// Função para exibir as informações dos armários
+// Função para exibir as informações dos armários e o sistema de marcação
 function exibirInformacoesArmarios() {
   const statusContainer = document.getElementById('status-container');
   statusContainer.innerHTML = `
@@ -29,26 +37,43 @@ function exibirInformacoesArmarios() {
     <p>Armários disponíveis: ${armariosDisponiveis.join(", ")}</p>
     <p>Armários indisponíveis: ${armariosIndisponiveis.join(", ")}</p>
   `;
-  statusContainer.style.display = 'block';
 
-  const botoesContainer = document.createElement('div');
-  botoesContainer.classList.add('botoes-container');
+  const sistemaMarcacao = document.createElement('div');
+  sistemaMarcacao.classList.add('sistema-marcacao');
 
   armariosDisponiveis.forEach((armario) => {
-    const botao = document.createElement('button');
-    botao.textContent = `Marcar Armário ${armario} como Indisponível`;
-    botao.addEventListener('click', () => marcarArmarioIndisponivel(armario));
-    botoesContainer.appendChild(botao);
+    const botao = criarBotaoMarcacao(armario, "disponivel");
+    sistemaMarcacao.appendChild(botao);
   });
 
   armariosIndisponiveis.forEach((armario) => {
-    const botao = document.createElement('button');
-    botao.textContent = `Marcar Armário ${armario} como Disponível`;
-    botao.addEventListener('click', () => marcarArmarioDisponivel(armario));
-    botoesContainer.appendChild(botao);
+    const botao = criarBotaoMarcacao(armario, "indisponivel");
+    sistemaMarcacao.appendChild(botao);
   });
 
-  statusContainer.appendChild(botoesContainer);
+  statusContainer.appendChild(sistemaMarcacao);
+}
+
+// Função para criar um botão de marcação de armário
+function criarBotaoMarcacao(armario, status) {
+  const botao = document.createElement('button');
+  botao.textContent = `Armário ${armario}`;
+  botao.classList.add(status);
+
+  if (status === "disponivel") {
+    botao.addEventListener('click', () => {
+      marcarArmarioIndisponivel(armario);
+    });
+  } else if (status === "indisponivel") {
+    botao.addEventListener('click', () => {
+      const confirmacao = window.confirm("Tem certeza de que deseja marcar este armário como disponível?");
+      if (confirmacao) {
+        marcarArmarioDisponivel(armario);
+      }
+    });
+  }
+
+  return botao;
 }
 
 // Função para marcar um armário como indisponível
