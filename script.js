c33598ee5bea824b9d273172e3a30bbe58b815b9
DaniloBM1 <140429439+DaniloBM1@users.noreply.github.com>
2023-07-24 15:49:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,71 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Números de matrículas autorizadas e seus respectivos nomes
+document.addEventListener('DOMContentLoaded', function() {
   const alunos = {
     "297684550674": "João da Silva",
     "212210605005": "Maria Souza",
     "238946972429": "Pedro Almeida"
   };
 
-  // Variável para armazenar os números dos armários disponíveis e indisponíveis
   let armariosDisponiveis = [1, 2, 5, 10, 15];
   let armariosIndisponiveis = [3, 4, 6, 8, 12];
 
-  // Variável para armazenar o número de matrícula selecionado para marcar disponível para indisponível
   let matriculaSelecionada;
 
-  // Função para validar o número de matrícula
   function validarMatricula() {
     const matriculaInput = document.getElementById('matricula');
     const matricula = matriculaInput.value.trim();
 
     if (alunos.hasOwnProperty(matricula)) {
-      // Número de matrícula válido
       matriculaSelecionada = matricula;
-      matriculaInput.value = ''; // Limpar o campo de entrada
+      matriculaInput.value = '';
       exibirInformacoesArmarios();
     } else {
-      // Número de matrícula inválido
       alert("Matrícula inválida. Por favor, tente novamente.");
     }
   }
 
-  // Restante do código permanece o mesmo...
-  // ...
+  function marcarDesmarcarArmario(armario) {
+    const indexDisponivel = armariosDisponiveis.indexOf(armario);
+    const indexIndisponivel = armariosIndisponiveis.indexOf(armario);
+
+    if (indexDisponivel !== -1) {
+      armariosDisponiveis.splice(indexDisponivel, 1);
+      armariosIndisponiveis.push(armario);
+    } else if (indexIndisponivel !== -1) {
+      const confirmacao = confirm("Deseja marcar este armário como disponível?");
+      if (confirmacao) {
+        armariosIndisponiveis.splice(indexIndisponivel, 1);
+        armariosDisponiveis.push(armario);
+      }
+    }
+
+    exibirInformacoesArmarios();
+  }
+
+  function exibirInformacoesArmarios() {
+    const statusContainer = document.getElementById('status-container');
+    statusContainer.innerHTML = "";
+
+    for (let i = 1; i <= 15; i++) {
+      const btn = document.createElement('button');
+      btn.textContent = `Armário ${i}`;
+
+      if (armariosDisponiveis.includes(i)) {
+        btn.classList.add('disponivel');
+        btn.addEventListener('click', () => marcarDesmarcarArmario(i));
+      } else {
+        btn.classList.add('indisponivel');
+      }
+
+      statusContainer.appendChild(btn);
+    }
+
+    const nomeAluno = alunos[matriculaSelecionada];
+    if (nomeAluno) {
+      const nomeAlunoContainer = document.createElement('div');
+      nomeAlunoContainer.textContent = `Aluno(a): ${nomeAluno}`;
+      statusContainer.appendChild(nomeAlunoContainer);
+    }
+  }
+
+  document.getElementById('btn-acessar').addEventListener('click', validarMatricula);
 });
